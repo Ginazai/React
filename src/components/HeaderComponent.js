aa986b0e795c7613de4bb71ad7490214a238b8c0
Ginazai <rafaeldc1300@gmail.com>
2023-12-26 20:53:49 -0500

    added Footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class Header extends Component {
+
+	render() {
+		return(
+			<React.Fragment>
+		        <Navbar dark color="primary">
+		          <div className="container">
+		            <NavbarBrand href="/">
+		              Ristorante Con Fusion
+		            </NavbarBrand>
+		          </div>
+		        </Navbar>
+			</React.Fragment>
+			);
+	}
+
+}
+export default Header;
